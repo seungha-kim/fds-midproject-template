6db8616b8ace2ec842fc1092f24eb6b4c6571f23
Kim Seungha <seungha.me@gmail.com>
2018-05-22 21:14:27 +0900

    로그인 기능, 게시글 작성 기능

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import './index.scss';
-import axiosDefault from 'axios';
+import axios from 'axios';
+
+axios.defaults.validateStatus = () => true;
 
 const baseURL = process.env.API_BASE_URL;
 console.log(baseURL);
 
-let axios = axiosDefault.create({
-  baseURL
+let postApi = axios.create({
+  baseURL,
 });
 
 const root = document.querySelector('#root');
 
 function render(el) {
   root.textContent = '';
+  console.log(el);
   root.appendChild(el);
 }
 
@@ -27,28 +30,70 @@ function interpolate(el, obj) {
 const templates = {
   index: document.querySelector('#index').content,
   indexTr: document.querySelector('#index-tr').content,
-  postForm: document.querySelector('#post-form').content,
+  newPost: document.querySelector('#new-post').content,
   postView: document.querySelector('#post-view').content,
+  login: document.querySelector('#login').content,
 }
 
-async function indexTable() {
-  const {data: posts} = await axios.get('/posts');
+// RORO pattern https://medium.freecodecamp.org/elegant-patterns-in-modern-javascript-roro-be01e7669cbd
+async function index({page = 1} = {}) {
+  const {data: posts} = await postApi.get(`/posts?_page=${page}`);
   const indexEl = document.importNode(templates.index, true);
   const tbodyEl = indexEl.querySelector('.index__tbody');
   posts.forEach(({id, title}) => {
     const trEl = document.importNode(templates.indexTr, true);
-    interpolate(trEl, {id, title, author: 'asdf'});
+    interpolate(trEl, {id, title, author: '익명'});
     tbodyEl.appendChild(trEl);
   })
   indexEl.querySelector('.index__new-post').addEventListener('click', e => {
-    postForm();
+    newPost();
+  })
+  indexEl.querySelector('.index__login-button').addEventListener('click', e => {
+    login();
   })
   render(indexEl);
 }
 
-indexTable();
-
-async function postForm() {
-  const postFormEl = document.importNode(templates.postForm, true);
+async function newPost() {
+  const postFormEl = document.importNode(templates.newPost, true);
+  postFormEl.querySelector('.new-post__cancel').addEventListener('click', e => {
+    e.preventDefault();
+    index();
+  })
+  postFormEl.querySelector('.new-post__form').addEventListener('submit', async e => {
+    e.preventDefault();
+    const payload = {
+      title: e.target.elements.title.value,
+      body: e.target.elements.body.value,
+    };
+    const res = await postApi.post('/posts', payload);
+    if (res.status === 401) {
+      alert('로그인이 되지 않았습니다.');
+    } else {
+      index();
+    }
+  })
   render(postFormEl);
 }
+
+async function login() {
+  const loginEl = document.importNode(templates.login, true);
+  loginEl.querySelector('.login__form').addEventListener('submit', async e => {
+    e.preventDefault();
+    const payload = {
+      username: e.target.elements.username.value,
+      password: e.target.elements.password.value,
+    };
+    const res = await postApi.post('/users/login', payload);
+    if (res.data.token) {
+      localStorage.setItem('token', res.data.token);
+      postApi.defaults.headers['Authorization'] = `Bearer ${res.data.token}`;
+      index();
+    } else {
+      alert('로그인을 실패했습니다. 다시 시도해보세요.');
+    }
+  })
+  render(loginEl);
+}
+
+index();
